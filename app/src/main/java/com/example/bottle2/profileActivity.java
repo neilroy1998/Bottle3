2a2957a958512d532ec5caf021ad161c837bec5d
neilroy1998 <neilroy1998@gmail.com>
2019-04-03 20:13:35 +0530

    Added Profile Activity

---
+++ app/src/main/java/com/example/bottle2/profileActivity.java
@@ -0,0 +1,21 @@
+package com.example.bottle2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class profileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void person_assign_tasks(View v) {
+
+    }
+
+    public void person_assign_tasks(View view) {
+    }
+}
